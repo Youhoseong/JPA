4eceb09fbcce5f3eefbe379661788e78df871e6d
Youhoseong <h0song@naver.com>
2022-01-17 19:03:58 +0900

    [Feat] 컬렉션을 DTO로 바로 조회하기(최적화X)

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -9,12 +9,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(of = "orderId")
-public class OrderQueryDto {
 
+public class OrderQueryDto {
     private Long orderId;
     private String name;
-    private LocalDateTime orderDate; //주문시간
+    private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderItems;
@@ -25,14 +24,5 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
-    }
-
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
-        this.orderId = orderId;
-        this.name = name;
-        this.orderDate = orderDate;
-        this.orderStatus = orderStatus;
-        this.address = address;
-        this.orderItems = orderItems;
     }
 }
